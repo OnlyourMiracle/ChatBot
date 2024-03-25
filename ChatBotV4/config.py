88a3fe81e66cba7f529182d17ef886b444720542
OnlyourMiracle <2440310664@qq.com>
2024-03-25 15:33:04 +0800

    Update configV8.py

--- ChatBotV4/config.py
+++ ChatBotV4/config.py
@@ -11,11 +11,11 @@ class Config:
     max_input_length = 50 #输入的最大句子长度
     max_generate_length = 20 #生成的最大句子长度
     prefix = '/home/aistudio/ChatBot/ChatBotV2/Chinese-Chatbot-PyTorch-Implementation/Newcheckpoints/chatbot'  #模型断点路径前缀
-    model_ckpt  = '/home/aistudio/ChatBot/ChatBotV2/Chinese-Chatbot-PyTorch-Implementation/Newcheckpoints/chatbot_0325_1119'
+    model_ckpt  = '/home/aistudio/ChatBot/ChatBotV2/Chinese-Chatbot-PyTorch-Implementation/Newcheckpoints/chatbot_0325_1522'
     '''
     训练超参数
     '''
-    batch_size = 2048
+    batch_size = 4096
     shuffle = True #dataloader是否打乱数据
     num_workers = 0 #dataloader多进程提取数据
     bidirectional = True #Encoder-RNN是否双向
@@ -25,7 +25,7 @@ class Config:
     dropout = 0 #是否使用dropout
     clip = 50.0 #梯度裁剪阈值
     num_layers = 2 #Encoder-RNN层数
-    learning_rate = 0.0001
+    learning_rate = 0.001
     teacher_forcing_ratio = 1.0 #teacher_forcing比例
     decoder_learning_ratio = 5.0
     '''
